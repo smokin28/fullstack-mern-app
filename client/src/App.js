cc8e40a9374b87d4c5db60e606a766ec4843b6b7
Przemyslaw Bobkowski <przemek.bobkowski@icloud.com>
2022-12-26 16:09:15 -0500

    Typing away at client...

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,33 @@
+import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { useMemo } from 'react'
+import { useSelector } from 'react-redux'
+import { createTheme } from '@mui/material/styles'
+import { themeSettings } from './theme'
+
+import HomePage from './scenes/HomePage'
+import LoginPage from './scenes/loginPage/LoginPage'
+import ProfilePage from './scenes/ProfilePage'
+
 export default function App() {
+  const mode = useSelector((state) => state.mode)
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
+  const isAuth = Boolean(useSelector((state) => state.token))
+
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <ThemeProvider theme={ theme }>
+          <CssBaseline />
+          <Routes>
+            <Route path='/' element={ <LoginPage /> } />
+            <Route path='/home'
+                   element={ isAuth ? <HomePage /> : <Navigate to='/' /> } />
+            <Route path='/profile/:userId'
+                   element={ isAuth ? <ProfilePage /> : <Navigate to='/' /> } />
+          </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   )
 }
